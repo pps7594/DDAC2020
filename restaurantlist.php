d9905fda902aaa630e667c87de6a8b7dfa979dbe
pps7594 <58364431+pps7594@users.noreply.github.com>
2020-06-10 12:08:47 +0800

    Update restaurantlist.php

--- restaurantlist.php
+++ restaurantlist.php
@@ -1,3 +1,16 @@
+<?php
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "pps7594", "pwd" => "{Google7594220}", "Database" => "WebAppDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:tp042049testdb.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if(!$conn){
+  die("Error Connect :".sqlsrv_errors();
+}
+      echo "Good";
+?>
+
 <html>
 <head>
 <title>List of KL Restaurant</title>
@@ -22,4 +35,4 @@
 </table>
 </center>
 </body>
-</html
+</html>
